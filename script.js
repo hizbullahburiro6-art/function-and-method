a702602dbe060193bece76c6b3e62292acd8e012
hizbullahburiro6-art <hizbullahburiro6@gmail.com>
2026-02-07 21:29:22 +0500

    fix:

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-// function myFunction(){
-//     console.log("code with Hizbullah")
+// function myFunction(maseeg){
+//      console.log(maseeg)
 // }
 
+// myFunction("i love js");
 
-// myFunction();
 
 
-function sum(a,b){
-    console.log(a+b)
+// function sum(y,z){
+//      z = y+z;
+//      console.log('before return')
+//      return z;
+//      confirm.log("after return")
+
+// }
+
+// let val = sum(23, 72)
+// console.log(val)
+
+
+function sum(y,z){
+     z = y*z
+     return z;
+
 }
+
+
+
+ let val = sum(24,85)
+ console.log(val)
+
+
+ const arry = (a,b) =>{
+     console.log(a + b)
+ }
+
+ 
